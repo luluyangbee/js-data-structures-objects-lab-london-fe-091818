3cdd212d50f11077ecdc4e4b38f64481be84d8bd
Lulu Yang <lulu.yang@wework.com>
2018-10-24 09:47:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+
+const driver = ["driver"];
+
+
+function updateDriverWithKeyAndValue(driver, key, value){
+   driver[key] = "name: 'Sam'";
+   driver["value"] = "address: '11 Broadway'";
+   return driver
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
+  driver[key] = "name: 'Sam'";
+  driver["value"] = "address: '12 Broadway'";
+  return driver
+}
+
+function deleteFromDriverByKey(driver, key){
+  
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key){
+  
+}
+
